6d4430b01b18eee41f60061d2b542a356afa6241
MasBanter <andreas@students.amikom.ac.id>
2024-12-26 15:07:48 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,12 +1,7 @@
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, NULL, NULL);
-mysqli_real_connect($conn, "scc-server.mysql.database.azure.com", "nsruuvnlvc", "Putra.123.", "computerscc", 3306, MYSQLI_CLIENT_SSL);
+$con = mysqli_connect('scc-server.mysql.database.azure.com', 'nsruuvnlvc', 'Putra.123.', 'computerscc');
 if (!$con) {
     echo "fail";
     die(mysqli_error($con));
 }
 ?>
-
-
-
