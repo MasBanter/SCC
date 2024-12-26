0a8f5e20275c93f95b0f3a1d5b83ed6cc1101ad6
MasBanter <andreas@students.amikom.ac.id>
2024-12-26 16:14:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
 <?php
 session_start();
 
-// Include the database connection file
-include('connect.php');
-
-// Fetch 5 highlighted products
-function getHighlightedProducts() {
-    global $conn; // Use the connection from connect.php
-
-    $sql = "SELECT * FROM products WHERE is_highlighted = 1 LIMIT 5";
-    $result = $conn->query($sql);
-
-    $products = [];
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $products[] = $row;
-        }
-    }
-
-    return $products;
-}
-
-// Call the function to fetch the highlighted products
-$highlightedProducts = getHighlightedProducts();
+if (empty($_SESSION['aid']))
+    $_SESSION['aid'] = -1;
 ?>
 
 <!DOCTYPE html>
@@ -34,12 +14,17 @@ $highlightedProducts = getHighlightedProducts();
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Shop Component Computer</title>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+
     <link rel="stylesheet" href="style.css" />
+
+
 </head>
 
 <body>
     <section id="header">
         <a href="index.php"><img src="img/logo.png" class="logo" alt="" /></a>
+
         <div>
             <ul id="navbar">
                 <li><a class="active" href="index.php">Home</a></li>
@@ -48,30 +33,70 @@ $highlightedProducts = getHighlightedProducts();
                 <li><a href="contact.php">Contact</a></li>
 
                 <?php
+
                 if ($_SESSION['aid'] < 0) {
-                    echo "<li><a href='login.php'>login</a></li><li><a href='signup.php'>SignUp</a></li>";
+                    echo "   <li><a href='login.php'>login</a></li>
+            <li><a href='signup.php'>SignUp</a></li>
+            ";
                 } else {
-                    echo "<li><a href='profile.php'>profile</a></li>";
+                    echo "   <li><a href='profile.php'>profile</a></li>
+          ";
                 }
                 ?>
                 <li><a href="admin.php">Admin</a></li>
-                <li id="lg-bag"><a href="cart.php"><i class="far fa-shopping-bag"></i></a></li>
+                <li id="lg-bag">
+                    <a href="cart.php"><i class="far fa-shopping-bag"></i></a>
+                </li>
+                <a href="#" id="close"><i class="far fa-times"></i></a>
             </ul>
+        </div>
+        <div id="mobile">
+            <a href="cart.php"><i class="far fa-shopping-bag"></i></a>
+            <i id="bar" class="fas fa-outdent"></i>
         </div>
     </section>
 
-    <section id="highlighted-products" class="section-p1">
-        <h2>Featured Products</h2>
-        <div class="product-container">
-            <?php foreach ($highlightedProducts as $product): ?>
-                <div class="product-box">
-                    <img src="img/products/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" />
-                    <h4><?php echo $product['name']; ?></h4>
-                    <p><?php echo $product['description']; ?></p>
-                    <span>Rp <?php echo number_format($product['price'], 0, ',', '.'); ?></span>
-                    <a href="product-detail.php?id=<?php echo $product['id']; ?>">View Details</a>
-                </div>
-            <?php endforeach; ?>
+    <section id="hero">
+        <h4>Halo! Selamat Datang</h4>
+        <h2>Di Shop Component Computer</h2>
+        <p>Menyediakan berbagai macam komponen komputer berkualitas tinggi untuk kebutuhan rakit PC, upgrade perangkat, serta perawatan komputer Anda. Kami menyediakan berbagai produk dari merek terpercaya, mulai dari prosesor, kartu grafis, motherboard. Selain itu, kami juga menawarkan berbagai aksesori komputer, pendingin sistem, dan peralatan lainnya yang dapat memenuhi kebutuhan gaming, desain grafis, serta komputasi berat.</p>
+        <a href="shop.php">
+            <button>Shop Now</button>
+        </a>
+    </section>
+
+    <section id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="img/features/f1.png" alt="" />
+            <h6>Pengiriman Gratis</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f2.png" alt="" />
+            <h6>Online Order</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f6.png" alt="" />
+            <h6>CS 24/7 </h6>
+        </div>
+    </section>
+
+
+    <section id="sm-banner" class="section-p1">
+        <div class="banner-box">
+            <h4>crazy deals</h4>
+            <h2>Beli kombo, dapatkan satu aksesori gratis</h2>
+            <span>Klasik terbaik sedang dijual di SCC</span>
+            <a href="shop.php">
+                <button class="white">Learn More</button>
+            </a>
+        </div>
+        <div class="banner-box banner-box2">
+            <h4>Coming This Week</h4>
+            <h2>Ragnar Sale</h2>
+            <span>Klasik terbaik sedang dijual di SCC</span>
+            <a href="shop.php">
+                <button class="white">Collection</button>
+            </a>
         </div>
     </section>
 
@@ -79,13 +104,26 @@ $highlightedProducts = getHighlightedProducts();
         <div class="col">
             <img class="logo" src="img/logo.png" />
             <h4>Contact</h4>
-            <p><strong>Address: </strong> Jln. Palagan, Sleman, Yogyakarta</p>
-            <p><strong>Phone: </strong> +62 812 3456 7891</p>
+            <p>
+                <strong>Address: </strong> Jln. Palagan, Sleman, Yogyakarta
+
+            </p>
+            <p>
+                <strong>Phone: </strong> +62 812 3456 7891
+            </p>
+            <p>
+                <strong>Hours: </strong> Senin hingga Sabtu: pukul 09.00 hingga 17.00
+            </p>
         </div>
+
         <div class="col">
             <h4>Akun Saya</h4>
             <a href="cart.php">Lihat Keranjang</a>
             <a href="wishlist.php">Daftar Keinginan Saya</a>
+        </div>
+        <div class="col install">
+            <p>Pembayaran Aman</p>
+            <img src="img/pay/pay.png" />
         </div>
         <div class="copyright">
             <p>2024. Kelompok 5 CC</p>
@@ -96,3 +134,12 @@ $highlightedProducts = getHighlightedProducts();
 </body>
 
 </html>
+
+<script>
+window.addEventListener("onunload", function() {
+  // Call a PHP script to log out the user
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "logout.php", false);
+  xhr.send();
+});
+</script>
