cd6fe1347047982afe591e21ed878d3edd28c5b3
MasBanter <andreas@students.amikom.ac.id>
2024-12-26 15:10:32 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,7 +1,12 @@
 <?php
-$con = mysqli_connect('scc-server.mysql.database.azure.com', 'nsruuvnlvc', 'Putra.123.', 'computerscc');
+// Melakukan koneksi ke database tanpa SSL
+$con = mysqli_connect("scc-server.mysql.database.azure.com", "nsruuvnlvc", "Putra.123.", "computerscc", 3306);
+
 if (!$con) {
     echo "fail";
     die(mysqli_error($con));
 }
+
+// Jika koneksi berhasil
+echo "Koneksi berhasil!";
 ?>
