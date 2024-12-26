6581e02bd01525245ee8dfa0b09a6d6d2b9d11c4
MasBanter <andreas@students.amikom.ac.id>
2024-12-26 14:16:42 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '', 'computer_shop');
+$con = mysqli_connect('scc-server.mysql.database.azure.com', 'nsruuvnlvc', 'Putra.123.', 'computer_shop');
 if (!$con) {
     echo "fail";
     die(mysqli_error($con));
