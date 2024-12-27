df304429776566c25da47f256da7c2593ff16971
erixngrh26 <erixml.2004@gmail.com>
2024-12-27 22:52:06 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -4,47 +4,66 @@ session_start();
 if (isset($_POST['sub'])) {
     include("include/connect.php");
 
-    if (!isset($_SESSION['aid']) || !is_int($_SESSION['aid'])) {
-        header("Location: login.php");
-        exit();
-    }
-    
     $aid = $_SESSION['aid'];
-    $add = mysqli_real_escape_string($con, $_POST['houseadd']);
-    $city = mysqli_real_escape_string($con, $_POST['city']);
-    $country = mysqli_real_escape_string($con, $_POST['country']);
-    $acc = isset($_POST['acc']) ? mysqli_real_escape_string($con, $_POST['acc']) : null;
-    
+    $add = $_POST['houseadd'];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
+    $acc = $_POST['acc'];
     $query = "";
-    
+
     if (empty($acc)) {
-        $query = "INSERT INTO `orders` (dateod, datedel, aid, address, city, country, account, total) 
-                  VALUES (CURDATE(), NULL, '$aid', '$add', '$city', '$country', NULL, 0)";
+        $query = "insert into `orders` (dateod, datedel, aid, address, city, country, account, total) values(CURDATE(), NULL, '$aid', '$add', '$city', '$country', NULL, 0)";
     } else {
         if (preg_match('/\D/', $acc) || strlen($acc) != 16) {
-            echo "<script> 
-                    alert('Invalid account number'); 
-                    setTimeout(function(){ window.location.href = 'checkout.php'; }, 100); 
-                  </script>";
+            echo "<script> alert('invalid account number'); setTimeout(function(){ window.location.href = 'checkout.php'; }, 100); </script>";
             exit();
         }
-        $query = "INSERT INTO `orders` (dateod, datedel, aid, address, city, country, account, total) 
-                  VALUES (CURDATE(), NULL, '$aid', '$add', '$city', '$country', '$acc', 0)";
+
+        $query = "insert into `orders` (dateod, datedel, aid, address, city, country, account, total) values(CURDATE(), NULL, '$aid', '$add', '$city', '$country', '$acc', 0)";
     }
-    
-    if (!mysqli_query($con, $query)) {
-        echo "Error executing query: " . mysqli_error($con);
-        exit();
-    }
-    
+    $result = mysqli_query($con, $query);
+
     $oid = mysqli_insert_id($con);
-    
-    // Proceed with inserting into `order-details` and updating the cart...
-    // Redirect to profile page
+
+    $query = "SELECT * FROM cart JOIN products ON cart.pid = products.pid WHERE aid = $aid";
+
+    $result = mysqli_query($con, $query);
+    global $tott;
+    while ($row = mysqli_fetch_assoc($result)) {
+        $pid = $row['pid'];
+        $pname = $row['pname'];
+        $desc = $row['description'];
+        $qty = $row['qtyavail'];
+        $price = $row['price'];
+        $cat = $row['category'];
+        $img = $row['img'];
+        $brand = $row['brand'];
+        $cqty = $row['cqty'];
+        $tott = $price * $cqty;
+
+        $query = "insert into `order-details` (oid, pid, qty) values ($oid, $pid, $cqty)";
+
+        mysqli_query($con, $query);
+
+        $query = "update products set qtyavail = qtyavail - $cqty where pid = $pid";
+
+        mysqli_query($con, $query);
+    }
+
+    $query = "delete from cart where aid = $aid";
+
+    mysqli_query($con, $query);
+
+    $query = "update orders set total = $tott where oid = $oid";
+
+    mysqli_query($con, $query);
+
+
     header("Location: profile.php");
     exit();
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -214,28 +233,28 @@ if (isset($_POST['sub'])) {
             <img class="logo" src="img/logo.png" />
             <h4>Contact</h4>
             <p>
-                <strong>Address: </strong> Jln. Palagan, Sleman, Yogyakarta
+                <strong>Address: </strong> Street 2, Johar Town Block A,Lahore
 
             </p>
             <p>
-                <strong>Phone: </strong> +62 812 3456 7891
+                <strong>Phone: </strong> +92324953752
             </p>
             <p>
-                <strong>Hours: </strong> Senin hingga Sabtu: pukul 09.00 hingga 17.00
+                <strong>Hours: </strong> 9am-5pm
             </p>
         </div>
 
         <div class="col">
-            <h4>Akun Saya</h4>
-            <a href="cart.php">Lihat Keranjang</a>
-            <a href="wishlist.php">Daftar Keinginan Saya</a>
+            <h4>My Account</h4>
+            <a href="cart.php">View Cart</a>
+            <a href="wishlist.php">My Wishlist</a>
         </div>
         <div class="col install">
-            <p>Pembayaran Aman</p>
+            <p>Secured Payment Gateways</p>
             <img src="img/pay/pay.png" />
         </div>
         <div class="copyright">
-            <p>2024. Kelompok 5 CC</p>
+            <p>2021. byteBazaar. HTML CSS </p>
         </div>
     </footer>
 
