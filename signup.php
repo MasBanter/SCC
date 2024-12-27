4d0d0cb88fdddad3fe5a197c482077d4b2beb80d
erixngrh26 <erixml.2004@gmail.com>
2024-12-27 22:00:38 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -63,24 +63,28 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ByteBazaar</title>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+
+    <link rel="stylesheet" href="style.css" />
+
 </head>
 
 <body>
     <section id="header">
-        <a href="#"><img src="img/logo.png" class="logo" alt=""></a>
+        <a href="#"><img src="img/logo.png" class="logo" alt="" /></a>
+
         <div>
             <ul id="navbar">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="shop.php">Shop</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
-                <li><a href="login.php">Login</a></li>
+                <li><a href="login.php">login</a></li>
                 <li><a class="active" href="signup.php">SignUp</a></li>
                 <li><a href="admin.php">Admin</a></li>
                 <li id="lg-bag">
@@ -95,61 +99,73 @@ if (isset($_POST['submit'])) {
         </div>
     </section>
 
-    <form method="post" action="signup.php" id="form">
+
+    <form method="post" id="form">
         <h3 style="color: darkred; margin: auto"></h3>
-        <input class="input1" id="fn" name="firstName" type="text" placeholder="First Name *" required>
-        <input class="input1" id="ln" name="lastName" type="text" placeholder="Last Name *" required>
-        <input class="input1" id="user" name="username" type="text" placeholder="Username *" required>
-        <input class="input1" id="email" name="email" type="email" placeholder="Email *" required>
-        <input class="input1" id="pass" name="password" type="password" placeholder="Password *" required>
-        <input class="input1" id="cpass" name="confirmPassword" type="password" placeholder="Confirm Password *" required>
-        <input class="input1" id="cnic" name="cnic" type="text" placeholder="CNIC *" pattern="[0-9]{13}" required>
-        <input class="input1" id="dob" name="dob" type="date" required>
-        <input class="input1" id="contact" name="phone" type="text" placeholder="Contact *" pattern="[0-9]+" required>
-        <select class="select1" id="gen" name="gender" required>
-            <option value="" disabled selected>Select Gender</option>
+        <input class="input1" id="fn" name="firstName" type="text" placeholder="First Name *" required="required">
+        <input class="input1" id="ln" name="lastName" type="text" placeholder="Last Name *" required="required">
+        <input class="input1" id="user" name="username" type="text" placeholder="Username *" required="required">
+        <input class="input1" id="email" name="email" type="text" placeholder="Email *" required="required">
+        <input class="input1" id="pass" name="password" type="password" placeholder="Password *" required="required">
+        <input class="input1" id="cpass" name="confirmPassword" type="password" placeholder="Confirm Password *"
+            required="required">
+        <input class="input1" id="cnic" name="cnic" type="number" placeholder="CNIC *" required="required">
+        <input class="input1" id="dob" name="dob" type="date" placeholder="Date Of Birth " required="required">
+        <input class="input1" id="contact" name="phone" type="number" placeholder="Contact *" required="required">
+        <select class="select1" id="gen" name="gender" required="required">
+            <option value="S">Select Gender</option>
             <option value="M">Male</option>
             <option value="F">Female</option>
         </select>
         <button name="submit" type="submit" class="btn">Submit</button>
+
     </form>
 
     <div class="sign">
-        <a href="login.php" class="sign">Already have an account?</a>
+        <a href="login.php" class="signn">Already have an account?</a>
     </div>
+
 
     <footer class="section-p1">
         <div class="col">
-            <img class="logo" src="img/logo.png">
+            <img class="logo" src="img/logo.png" />
             <h4>Contact</h4>
-            <p><strong>Address: </strong> Jln. Palagan, Sleman, Yogyakarta</p>
-            <p><strong>Phone: </strong> +62 812 3456 7891</p>
-            <p><strong>Hours: </strong> Senin hingga Sabtu: pukul 09.00 hingga 17.00</p>
+            <p>
+                <strong>Address: </strong> Street 2, Johar Town Block A,Lahore
+
+            </p>
+            <p>
+                <strong>Phone: </strong> +92324953752
+            </p>
+            <p>
+                <strong>Hours: </strong> 9am-5pm
+            </p>
         </div>
+
         <div class="col">
-            <h4>Akun Saya</h4>
-            <a href="cart.php">Lihat Keranjang</a>
-            <a href="wishlist.php">Daftar Keinginan Saya</a>
+            <h4>My Account</h4>
+            <a href="cart.php">View Cart</a>
+            <a href="wishlist.php">My Wishlist</a>
         </div>
         <div class="col install">
-            <p>Pembayaran Aman</p>
-            <img src="img/pay/pay.png">
+            <p>Secured Payment Gateways</p>
+            <img src="img/pay/pay.png" />
         </div>
         <div class="copyright">
-            <p>2024. Kelompok 5 CC</p>
+            <p>2021. byteBazaar. HTML CSS </p>
         </div>
     </footer>
 
-    <script>
-        document.getElementById("form").addEventListener("submit", function (event) {
-            const password = document.getElementById("pass").value;
-            const confirmPassword = document.getElementById("cpass").value;
-            if (password !== confirmPassword) {
-                event.preventDefault();
-                alert("Passwords do not match!");
-            }
-        });
-    </script>
+    <script src="script.js"></script>
 </body>
 
 </html>
+
+<script>
+window.addEventListener("unload", function() {
+  // Call a PHP script to log out the user
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "logout.php", false);
+  xhr.send();
+});
+</script>
