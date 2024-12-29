e9792d904586a01892c3702a0a04b51848ce4bc8
MasBanter <andreas@students.amikom.ac.id>
2024-12-30 01:27:29 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,84 +1,119 @@
 <?php
 session_start();
 
+// Aktifkan error reporting untuk debugging
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Periksa apakah sesi valid
+if (!isset($_SESSION['aid'])) {
+    die("Session expired. Please log in again.");
+}
+
+// Fungsi untuk menghindari SQL Injection
+function sanitizeInput($input, $con) {
+    return mysqli_real_escape_string($con, trim($input));
+}
+
 if (isset($_POST['sub'])) {
     include("include/connect.php");
 
     $aid = $_SESSION['aid'];
-    $add = $_POST['houseadd'];
-    $city = $_POST['city'];
-    $country = $_POST['country'];
-    $acc = $_POST['acc'];
-    $query = "";
+    $add = sanitizeInput($_POST['houseadd'], $con);
+    $city = sanitizeInput($_POST['city'], $con);
+    $country = sanitizeInput($_POST['country'], $con);
+    $acc = sanitizeInput($_POST['acc'], $con);
+    $totalOrder = 0;
 
-    if (empty($acc)) {
-       $query = "INSERT into `orders` (dateod, datedel, aid, address, city, country, account, total) values(CURDATE(), NULL, '$aid', '$add', '$city', '$country', '$acc', 0)";
-
-    } else {
-        if (preg_match('/\D/', $acc) || strlen($acc) != 16) {
-            echo "<script> alert('invalid account number'); setTimeout(function(){ window.location.href = 'checkout.php'; }, 100); </script>";
-            exit();
-        }
-
-       $query = "INSERT into `orders` (dateod, datedel, aid, address, city, country, account, total) values(CURDATE(), NULL, '$aid', '$add', '$city', '$country', '$acc', 0)";
-
+    // Validasi input alamat
+    if (empty($add) || empty($city) || empty($country)) {
+        die("Address, City, and Country fields are required.");
     }
+
+    // Validasi nomor akun jika diisi
+    if (!empty($acc) && (preg_match('/\D/', $acc) || strlen($acc) != 16)) {
+        echo "<script>alert('Invalid account number. It must be a 16-digit number.');</script>";
+        echo "<script>window.location.href = 'checkout.php';</script>";
+        exit();
+    }
+
+    // Insert ke tabel orders
+    $query = "INSERT INTO `orders` (dateod, datedel, aid, address, city, country, account, total) 
+              VALUES (CURDATE(), NULL, '$aid', '$add', '$city', '$country', '$acc', 0)";
     $result = mysqli_query($con, $query);
 
+    if (!$result) {
+        die("Error inserting order: " . mysqli_error($con));
+    }
+
+    // Ambil ID pesanan yang baru dibuat
     $oid = mysqli_insert_id($con);
 
+    // Ambil data dari cart
     $query = "SELECT * FROM cart JOIN products ON cart.pid = products.pid WHERE aid = $aid";
-
     $result = mysqli_query($con, $query);
-    global $tott;
-    while ($row = mysqli_fetch_assoc($result)) {
-        $pid = $row['pid'];
-        $pname = $row['pname'];
-        $desc = $row['description'];
-        $qty = $row['qtyavail'];
-        $price = $row['price'];
-        $cat = $row['category'];
-        $img = $row['img'];
-        $brand = $row['brand'];
-        $cqty = $row['cqty'];
-        $tott = $price * $cqty;
 
-        $query = "insert into `order-details` (oid, pid, qty) values ($oid, $pid, $cqty)";
-
-        mysqli_query($con, $query);
-
-        $query = "update products set qtyavail = qtyavail - $cqty where pid = $pid";
-
-        mysqli_query($con, $query);
+    if (!$result) {
+        die("Error fetching cart data: " . mysqli_error($con));
     }
 
-    $query = "delete from cart where aid = $aid";
+    // Proses setiap item di cart
+    while ($row = mysqli_fetch_assoc($result)) {
+        $pid = $row['pid'];
+        $cqty = $row['cqty'];
+        $price = $row['price'];
+        $qtyAvail = $row['qtyavail'];
+        $totalItem = $price * $cqty;
 
-    mysqli_query($con, $query);
+        // Validasi stok
+        if ($qtyAvail < $cqty) {
+            die("Insufficient stock for product ID $pid.");
+        }
 
-    $query = "update orders set total = $tott where oid = $oid";
+        // Insert ke tabel order-details
+        $query = "INSERT INTO `order-details` (oid, pid, qty) VALUES ($oid, $pid, $cqty)";
+        if (!mysqli_query($con, $query)) {
+            die("Error inserting order details: " . mysqli_error($con));
+        }
 
-    mysqli_query($con, $query);
+        // Update stok produk
+        $query = "UPDATE products SET qtyavail = qtyavail - $cqty WHERE pid = $pid";
+        if (!mysqli_query($con, $query)) {
+            die("Error updating product stock: " . mysqli_error($con));
+        }
 
+        // Tambahkan ke total pesanan
+        $totalOrder += $totalItem;
+    }
 
+    // Hapus cart pengguna
+    $query = "DELETE FROM cart WHERE aid = $aid";
+    if (!mysqli_query($con, $query)) {
+        die("Error clearing cart: " . mysqli_error($con));
+    }
+
+    // Update total pada tabel orders
+    $query = "UPDATE orders SET total = $totalOrder WHERE oid = $oid";
+    if (!mysqli_query($con, $query)) {
+        die("Error updating order total: " . mysqli_error($con));
+    }
+
+    // Redirect ke halaman profile
     header("Location: profile.php");
     exit();
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ByteBazaar</title>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <link rel="stylesheet" href="style.css">
 
     <style>
         #account-field {
@@ -88,65 +123,47 @@ if (isset($_POST['sub'])) {
         .hidden {
             display: none;
         }
+
         .input11 {
-  display: block;
-  width: 80%;
-  margin: 40px auto;
-  padding: 10px 5px;
-  border: none;
-  border-bottom: 0.01rem dimgray solid;
-  outline: none;
-}
+            display: block;
+            width: 80%;
+            margin: 40px auto;
+            padding: 10px 5px;
+            border: none;
+            border-bottom: 0.01rem dimgray solid;
+            outline: none;
+        }
 
-.table12 {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  overflow: auto;
-}
-
-.table12 tr{
-    width: 100%;
-  overflow: auto;
-
-}
-
+        .table12 {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            overflow: auto;
+        }
     </style>
-
 </head>
 
 <body>
     <section id="header">
-        <a href="index.php"><img src="img/logo.png" class="logo" alt="" /></a>
-
+        <a href="index.php"><img src="img/logo.png" class="logo" alt=""></a>
         <div>
             <ul id="navbar">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="shop.php">Shop</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
-
-                <?php
-
-                if ($_SESSION['aid'] < 0) {
-                    echo "   <li><a href='login.php'>login</a></li>
-            <li><a href='signup.php'>SignUp</a></li>
-            ";
-                } else {
-                    echo "   <li><a href='profile.php'>profile</a></li>
-          ";
-                }
-                ?>
+                <?php if ($_SESSION['aid'] < 0): ?>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="signup.php">SignUp</a></li>
+                <?php else: ?>
+                    <li><a href="profile.php">Profile</a></li>
+                <?php endif; ?>
                 <li><a href="admin.php">Admin</a></li>
                 <li id="lg-bag">
                     <a href="cart.php"><i class="far fa-shopping-bag"></i></a>
                 </li>
                 <a href="#" id="close"><i class="far fa-times"></i></a>
             </ul>
-        </div>
-        <div id="mobile">
-            <a href="cart.php"><i class="far fa-shopping-bag"></i></a>
-            <i id="bar" class="fas fa-outdent"></i>
         </div>
     </section>
 
@@ -156,132 +173,28 @@ if (isset($_POST['sub'])) {
         </div>
         <div class="d-flex">
             <form method="post" id="form1">
-
-                <h3 style="color: darkred; margin: auto"></h3>
                 <input class="input11" type="text" name="houseadd" placeholder="Address" required>
                 <input class="input11" type="text" name="city" placeholder="City" required>
                 <input class="input11" type="text" name="country" placeholder="County/State" required>
                 <input class="input11" id="account-field" type="text" name="acc" placeholder="Account Number">
                 <div>
-                    <input class="input2" type="radio" id="ac1" name="dbt" value="cod" onchange="showInputBox()"> Cash
-                    on Delivery
+                    <input class="input2" type="radio" id="ac1" name="dbt" value="cod" onchange="showInputBox()"> Cash on Delivery
                 </div>
                 <div>
-                    <input class="input2" type="radio" id="ac2" name="dbt" value="bank" checked
-                        onchange="showInputBox()"> Paypal/Visa/MasterCard <span>
-                        <img src="img/pay/pay.png" alt="">
-                    </span>
+                    <input class="input2" type="radio" id="ac2" name="dbt" value="bank" checked onchange="showInputBox()"> Paypal/Visa/MasterCard
                 </div>
                 <button name="sub" type="submit" class="btn112">Place Order</button>
             </form>
-            <div class="Yorder">
-                <table class="table12">
-                    <tr class='tr1'>
-                        <th class='th1' colspan='2'>Your order</th>
-                    </tr>
-
-                    <?php
-                    include("include/connect.php");
-
-                    $aid = $_SESSION['aid'];
-
-                    $query = "SELECT * FROM cart JOIN products ON cart.pid = products.pid WHERE aid = $aid";
-
-                    $result = mysqli_query($con, $query);
-
-                    global $tot;
-
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $pid = $row['pid'];
-                        $pname = $row['pname'];
-                        $desc = $row['description'];
-                        $qty = $row['qtyavail'];
-                        $price = $row['price'];
-                        $cat = $row['category'];
-                        $img = $row['img'];
-                        $brand = $row['brand'];
-                        $cqty = $row['cqty'];
-                        $a = $price * $cqty;
-                        $tot = 0;
-
-                        echo "
-            
-            <tr class='tr1'>
-              <td class='td1'>$pname x $cqty(Qty)</td>
-              <td class='td1'>$a</td>
-            </tr>
-
-              ";
-                    }
-                    echo "
-            <tr class='tr1'>
-            <td class='td1'>Subtotal</td>
-            <td class='td1'>$$tot.00</td>
-          </tr>
-          <tr class='tr1'>
-            <td class='td1'>Shipping</td>
-            <td class='td1'>Free shipping</td>
-          </tr>";
-                    ?>
-
-
-                </table><br>
-            </div><!-- Yorder -->
         </div>
     </div>
 
-    <footer class="section-p1">
-        <div class="col">
-            <img class="logo" src="img/logo.png" />
-            <h4>Contact</h4>
-            <p>
-                <strong>Address: </strong> Jln. Palagan, Sleman, Yogyakarta
-
-            </p>
-            <p>
-                <strong>Phone: </strong> +62 812 3456 7891
-            </p>
-            <p>
-                <strong>Hours: </strong> Senin hingga Sabtu: pukul 09.00 hingga 17.00
-            </p>
-        </div>
-
-        <div class="col">
-            <h4>Akun Saya</h4>
-            <a href="cart.php">Lihat Keranjang</a>
-            <a href="wishlist.php">Daftar Keinginan Saya</a>
-        </div>
-        <div class="col install">
-            <p>Pembayaran Aman</p>
-            <img src="img/pay/pay.png" />
-        </div>
-        <div class="copyright">
-            <p>2024. Kelompok 5 CC</p>
-        </div>
-    </footer>
-
-    <script src="script.js"></script>
+    <script>
+        function showInputBox() {
+            var select = document.querySelector('#ac1');
+            var inputBox = document.getElementById("account-field");
+            inputBox.style.display = select.checked ? "none" : "block";
+        }
+    </script>
 </body>
 
 </html>
-
-<script>
-    function showInputBox() {
-        var select = document.querySelector('#ac1');
-        var inputBox = document.getElementById("account-field");
-        if (!select.checked) {
-            inputBox.style.display = "block";
-        } else {
-            inputBox.style.display = "none";
-        }
-    }
-</script>
-
-<script>
-window.addEventListener("unload", function() {
-  // Call a PHP script to log out the user
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "logout.php", false);
-  xhr.send();
-});
-</script>
